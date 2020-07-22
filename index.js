cd28efb4a82e79db9038716703a82490ce078a86
magrini <magrinibruna@usp.br>
2020-07-22 17:00:31 -0300

    Inicializado simulador

--- index.js
+++ index.js
@@ -1,1 +1,119 @@
-console.log("Olá mundo!");
+const MAPEAMENTO_DIRETO = "direto"
+const MAPEAMENTO_TOTALMENTE_ASSOCIATIVO = "associativo"
+const MAPEAMENTO_ASSOCIATIVO_POR_CONJUNTO = "conjunto"
+
+const QUANTIDADE_LINHAS_NA_CACHE = 8
+
+const cores = ["DarkGrey", "DarkBlue", "ForestGreen", "LightPink", "LightSalmon"]
+
+let memoriaCache = []
+function inicializarMemoriaCache() {
+    for(let i = 0; i < QUANTIDADE_LINHAS_NA_CACHE; i++) {
+        memoriaCache.push({
+            v: 0,
+            tag: "x",
+            conteudoMemoria: "x"
+        })
+    }
+}
+inicializarMemoriaCache();
+
+let quantidade_de_conjuntos = 2;
+let quantidade_linhas_por_conjuntos = 4;
+
+function selecionarMapeamento() {
+    funcaoDeMapeamento = document.getElementById("funcaoDeMapeamento").value;
+
+    switch(funcaoDeMapeamento) {
+        case MAPEAMENTO_DIRETO:
+            quantidade_de_conjuntos = QUANTIDADE_LINHAS_NA_CACHE;
+            quantidade_linhas_por_conjuntos = 1;
+            break;
+        case MAPEAMENTO_TOTALMENTE_ASSOCIATIVO:
+            quantidade_de_conjuntos = 1;
+            quantidade_linhas_por_conjuntos = QUANTIDADE_LINHAS_NA_CACHE;
+            break;
+        case MAPEAMENTO_ASSOCIATIVO_POR_CONJUNTO:
+            quantidade_de_conjuntos = 2;
+            quantidade_linhas_por_conjuntos = 4;
+            break;
+    }
+
+    inicializarMemoriaCache();
+}
+
+function criarMemoriaCache(callback) {
+    if(callback) callback();
+
+    let simulador = document.getElementById("simulador");
+
+    while(simulador.childNodes[2])
+        simulador.removeChild(simulador.lastChild)
+
+    let indexMemoriaCache = 0;
+    for(let i = 0; i < quantidade_de_conjuntos; i++) {
+        let tbody = document.createElement('tbody');
+
+        for (let j = 0; j < quantidade_linhas_por_conjuntos; j++){
+            let tr = document.createElement('tr');  
+
+            let borderStyle = `2px solid ${cores[i % cores.length]}`
+            
+            tr.style.border = borderStyle;
+        
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            let td3 = document.createElement('td');
+
+            td1.style.border = borderStyle;
+            td2.style.border = borderStyle;
+            td3.style.border = borderStyle;
+
+            let text1 = document.createTextNode(memoriaCache[indexMemoriaCache].v);
+            let text2 = document.createTextNode(memoriaCache[indexMemoriaCache].tag);
+            let text3 = document.createTextNode(memoriaCache[indexMemoriaCache].conteudoMemoria);
+        
+            td1.appendChild(text1);
+            td2.appendChild(text2);
+            td3.appendChild(text3);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+        
+            tbody.appendChild(tr);
+        }
+        
+        simulador.appendChild(tbody);
+    }
+}
+
+criarMemoriaCache(selecionarMapeamento);
+
+document.getElementById("funcaoDeMapeamento").onchange = function () {
+    criarMemoriaCache(selecionarMapeamento);
+}
+
+let buscarEndereco = document.getElementById("buscarEndereco")
+buscarEndereco.addEventListener("click", () => {
+    let endereco = document.getElementById("endereco").value
+    let conteudoMemoria = document.getElementById(endereco).innerHTML;
+
+    switch(funcaoDeMapeamento) {
+        case MAPEAMENTO_DIRETO:
+            console.log("Mapeamento direto")
+            break;
+        case MAPEAMENTO_TOTALMENTE_ASSOCIATIVO:
+            console.log("Mapeamento totalmente associativo")
+            break;
+        case MAPEAMENTO_ASSOCIATIVO_POR_CONJUNTO:
+            console.log("Mapeamento associativo por conjunto")
+            break;
+    }
+
+    criarMemoriaCache();
+
+    let processador = document.getElementById("processador");
+    processador.innerHTML = conteudoMemoria;
+})
+
+
